85867229d0e1f08e25bc2c5b563980072fb15424
AzionMC <70230064+MathisBruel@users.noreply.github.com>
2023-01-16 22:34:17 +0100

    update

--- src/js/javascript-recette.js
+++ src/js/javascript-recette.js
@@ -4,47 +4,113 @@ let divContener = document.getElementById('contener');
 // get id of recette in url
 let url = window.location.href;
 let id = url.split("=")[1];
+let swiper = document.getElementById('swiper-div');
 // get the json file db.json in src/database and put in variable
 let db = fetch('src/database/db.json')
   .then((response) => response.json())
   .then((json) => {
-        let recette = json.recettes[id-1];
-        let img = document.createElement('img');
-        let title = document.createElement('h1');
-        let description = document.createElement('p');
-        let ingredients = document.createElement('p');
-        let preparation = document.createElement('p');
-        let separator = document.createElement('img')
-        img.src = "src/img/" + recette.image[0];
-        separator.src = "src/img/decoration.svg";
-        img.alt = recette.name;
-        separator.alt = "separator";
-        title.innerHTML = recette.name;
-        description.innerHTML = recette.Description;
-        ingredients.innerHTML = recette.Ingredients;
-        preparation.innerHTML = recette.Steps;
-        // set in body
-        /*divContener.appendChild(title);
-        divContener.appendChild(document.createElement("br"));
-        divContener.appendChild(document.createElement("br"));
-        divContener.appendChild(separator);
-        divContener.appendChild(document.createElement("br"));
-        divContener.appendChild(document.createElement("br"));
-        divContener.appendChild(document.createElement("br"));
-        divContener.appendChild(document.createElement("br"));
-        
-        // create slider
-        let slider_2 = document.createElement('section');
-        let container_wide = document.createElement('div');
+    let recette = json.recettes[id - 1];
+    let title = document.getElementById('title');
+    title.innerHTML = recette.name;
+
+    recette.image.forEach((image) => {
+      let figure = document.createElement('figure');
+      let div = document.createElement('div');
+      let div2 = document.createElement('div');
+      div.classList.add('swiper-slide');
+  
+  
+  
+      let img = document.createElement('img');
+      img.src = "src/img/" + image;
+      img.alt = name;
+      div2.id = "slide"
+      div2.appendChild(img);
+      div2.appendChild(document.createElement("br"));
+      div2.appendChild(document.createElement("br"));
+  
+  
+      swiper.appendChild(div);
+      div.appendChild(figure);
+      figure.appendChild(div2);
+    })
 
 
 
-        divContener.appendChild(img);
-        divContener.appendChild(description);
-        divContener.appendChild(ingredients);
-        divContener.appendChild(preparation);*/
 
-      
+
+    /*
+
+    Table
+
+    */
+
+    let table = document.getElementById('tbody');
+
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.innerHTML = "Nb pers./Portion";
+    tr.appendChild(td);
+    table.appendChild(tr);
+    for (let i = 0; i < 5; i++) {
+      let td = document.createElement('td');
+      td.innerHTML = recette.pas * (i + 1);
+      tr.appendChild(td);
+      table.appendChild(tr);
+    }
+
+    recette.Ingredients.forEach((ingredient) => {
+      let tr = document.createElement('tr');
+      let td = document.createElement('td');
+      td.innerHTML = ingredient.name;
+      tr.appendChild(td);
+      for (let i = 0; i < 5; i++) {
+        let td = document.createElement('td');
+        td.innerHTML = ingredient.quantity * (i + 1) + " " + ingredient.unit;
+        tr.appendChild(td);
+      }
+      table.appendChild(tr);
+
+    })
+
+
+    new Swiper("#swiper-2", {
+      slidesPerView: 1,
+      centeredSlides: true,
+      spaceBetween: 24,
+      pagination: {
+        el: ("#swiper-2 .swiper-custom-pagination"),
+        clickable: true,
+        renderBullet: function (index, className) {
+          return `<div class=${className}>
+          <span class="number">${index + 1}</span>
+          <span class="line"></span>
+          </div>`;
+        }
+      },
+      lazyLoading: true,
+      loop: true,
+      keyboard: {
+        enabled: true,
+      },
+      navigation: {
+        nextEl: "#nav-right",
+        prevEl: "#nav-left"
+      },
+      autoplay: {
+        delay: 7000, // change ici la valeur delay
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        800: {
+          slidesPerView: 1.5
+        },
+        1400: {
+          slidesPerView: 1
+        }
+      }
+    });
+
   })
   .catch((error) => {
     console.log(error);
